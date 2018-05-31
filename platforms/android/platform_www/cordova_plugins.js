755a39406f0c3e352520fa82a1126e55978070e8
Ian Blenke <ian@blenke.com>
2018-05-31 11:43:50 -0400

    cordova plugin add cordova-plugin-geolocation

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -24,6 +24,20 @@ module.exports = [
     "merges": [
       "window.plugins.speechRecognition"
     ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
   }
 ];
 module.exports.metadata = 
@@ -31,9 +45,10 @@ module.exports.metadata =
 {
   "cordova-plugin-device": "2.0.2",
   "cordova-plugin-background-mode": "0.7.2",
+  "cordova-plugin-speechrecognition": "1.1.2",
   "cordova-plugin-whitelist": "1.3.3",
   "newrelic-cordova-plugin": "5.0.0",
-  "cordova-plugin-speechrecognition": "1.1.2"
+  "cordova-plugin-geolocation": "4.0.1"
 };
 // BOTTOM OF METADATA
 });
